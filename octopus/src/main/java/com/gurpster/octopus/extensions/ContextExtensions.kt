fda9e59fba353fcbe592744917d1c8fb398fbb2c
Williaan Lopes <williaanlopes@gmail.com>
2022-05-05 00:25:18 -0300

    version 1.0.6

--- octopus/src/main/java/com/gurpster/octopus/extensions/ContextExtensions.kt
+++ octopus/src/main/java/com/gurpster/octopus/extensions/ContextExtensions.kt
@@ -2,13 +2,12 @@ package com.gurpster.octopus.extensions
 
 import android.content.Context
 import android.net.ConnectivityManager
-import android.widget.Toast
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
+import android.os.Build
 import androidx.appcompat.app.AlertDialog
 import com.gurpster.octopus.R
 
-fun Context.toast(text: String) = Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
-
-fun Context.toast(text: String, duration: Int) = Toast.makeText(this, text, duration).show()
 
 fun Context.showAlertDialog(
     positiveButtonLable: String = getString(R.string.okay),
@@ -27,15 +26,6 @@ fun Context.showAlertDialog(
     alert.show()
 }
 
-// Toash extensions
-fun Context.showShotToast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-}
-
-fun Context.showLongToast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-}
-
 /**
  * isOnline{ // Do you work when connected with internet }
  + if you want to execute some code when there is no internet you can pass it as first lambda
@@ -47,14 +37,77 @@ fun Context.showLongToast(message: String) {
 */
 fun Context?.hasInternetConnection(failBlock : () -> Unit  = { globalInternetFailBock() }, successBlock : () -> Unit ) {
     this?.apply {
+//        val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+//        val netInfo = cm.activeNetworkInfo
+//        if (netInfo != null && netInfo.isConnected){
+//            successBlock()
+//        }else{
+//            failBlock()
+//        }
+
+        /*val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connectivityManager.activeNetwork
+            val actNw = connectivityManager.getNetworkCapabilities(networkCapabilities)
+            if (actNw != null) {
+                when {
+                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> successBlock()
+                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> successBlock()
+                    actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> successBlock()
+                    else -> failBlock()
+                }
+            } else failBlock()
+        } else {
+            connectivityManager.run {
+                connectivityManager.activeNetworkInfo?.run {
+                    when (type) {
+                        ConnectivityManager.TYPE_WIFI -> successBlock()
+                        ConnectivityManager.TYPE_MOBILE -> successBlock()
+                        ConnectivityManager.TYPE_ETHERNET -> successBlock()
+                        else -> failBlock()
+                    }
+
+                }
+            }
+        }*/
+
         val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val netInfo = cm.activeNetworkInfo
-        if (netInfo != null && netInfo.isConnected){
-            successBlock()
-        }else{
-            failBlock()
-        }
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+
+
     }?:failBlock()
+}
+
+enum class NetworkConnectivityType {
+    TYPE_MOBILE, TYPE_WIFI, TYPE_UNKNOWN
+}
+
+fun Context.internetType(): NetworkConnectivityType {
+    val connectivityMgr = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val nc = connectivityMgr.getNetworkCapabilities(connectivityMgr.activeNetwork)
+        when {
+            nc!!.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                NetworkConnectivityType.TYPE_MOBILE
+            }
+            nc.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                NetworkConnectivityType.TYPE_WIFI
+            }
+            else -> NetworkConnectivityType.TYPE_UNKNOWN
+        }
+    } else {
+        val activeNetwork: NetworkInfo? = connectivityMgr.activeNetworkInfo
+        when {
+            activeNetwork!!.type == ConnectivityManager.TYPE_WIFI -> {
+                NetworkConnectivityType.TYPE_WIFI
+            }
+            activeNetwork.type == ConnectivityManager.TYPE_MOBILE -> {
+                NetworkConnectivityType.TYPE_MOBILE
+            }
+            else -> NetworkConnectivityType.TYPE_UNKNOWN
+        }
+    }
 }
 
 fun Context?.globalInternetFailBock(){
